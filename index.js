9824e73409827be4ee4b3888e0226feebeb78f00
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-01-27 23:44:52 -0300

    Add first structure of the '/participants' route

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-import express from 'express';
+import express, { json } from 'express';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
 
 const server = express();
+
+server.use(cors());
+server.use(json());
+
+server.post('/participants', (req, res) => {
+
+    res.sendStatus(201);
+
+});
+
+server.get('/participants', (req, res) => {
+    console.log("qualé");
+    res.sendStatus(201);
+
+});
+
 
 server.listen(5000);
